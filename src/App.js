347c5ede44760ae06d9c4a67c6ec4977e6f020be
orikurt <orikball@gmail.com>
2018-06-16 21:15:30 +0300

    change player names dynamically

--- src/App.js
+++ src/App.js
@@ -7,25 +7,29 @@ class App extends Component {
 
   randomColor = ()=> `rgb(${this.rand255()}, ${this.rand255()}, ${this.rand255()})`
 
-  appTitle = {
-    color: this.randomColor()
+  appTitle = Object.assign({ color: this.randomColor()}, Styles.appTitle);
+
+  state = {
+    playerNames: ["MJ", "Kobe", "Steph", "Kawhi"],
+    teams: ["Bulls", "Lakers", "Jazz", "Warriors"],
+    newName: "LebRon"
   }
 
-  playerNames = ["MJ", "Kobe", "Steph", "Kawhi"]
+  clickHandler = () => this.setState({playerNames: [this.state.newName, "Kobe", "Steph", "Kawhi"]})
 
-  render() {
-    Object.assign(this.appTitle, Styles.appTitle);
+  render() {    
     return (
       <div style={Styles.app}>
         <header style={Styles.appHeader}>
           <h1 style={this.appTitle}>Stats Center</h1>
         </header>
         {
-          this.playerNames.map((name, i)=> ( 
+          this.state.playerNames.map((name, i)=> ( 
             <Player key={i} name={name} /> 
           ))
         }
-        <button></button>
+        <button onClick={this.clickHandler}>{this.state.newName} is better than MJ</button>
+        <input type="text" onChange={(event)=>this.setState({newName: event.target.value})} value={ this.state.newName } />
       </div>
     );
   }
