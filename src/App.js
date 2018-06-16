60dc9d9fb739420fe3c0a4021e1bb7b690c50c93
ofir aminov <aminovo3@gmail.com>
2018-06-16 21:18:35 +0300

    Add Click Functionality

--- src/App.js
+++ src/App.js
@@ -5,8 +5,20 @@ import Player from './Player/Player'
 
 class App extends Component {
 
+  state = {
+    playerNames: ["Kawhi", "Kobe", "Shaq", "MJ"],
+    newName: "Lebron"
+  }
 
-  playerNames = ["Kawhi", "Kobe", "Shaq", "MJ"];
+  
+
+  ClickHandler = () => {
+    this.setState(
+      {
+        playerNames: ["Kawhi", "Kobe", "Shaq", this.state.newName]
+      }
+    )
+  }
 
   render() {
     return (
@@ -16,11 +28,13 @@ class App extends Component {
           <h1 className="App-title">Stats Center</h1>
         </header>
         {
-          this.playerNames.map((name, i) => (
+          this.state.playerNames.map((name, i) => (
             <Player key={i} name={name} />
             ))
         }
 
+        <button onClick = {this.ClickHandler} >Change Players</button>
+        <input type="text" value={this.state.newName} onChange={(event)=>{this.setState({newName:event.target.value})}} />
       </div>
     );
   }
