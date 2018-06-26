4fae1c7c086523541cdcd08d37925e2ee96f2c69
orikurt <orikball@gmail.com>
2018-06-27 00:23:19 +0300

    add favicon and banner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,13 +3,22 @@ import Search from '../components/Search';
 
 const Home = () => (
     <div style={homeStyle}>
-        <h3>GameTime Market</h3>
+        <div style={bannerStyle}>
+            <img style={{width: '100%', height: '100%'}} src="banner.png" />
+        </div>
         <Search style={searchStyle}/>
     </div>
 )
 
 const homeStyle = {
-    textAlign: 'center'
+    textAlign: 'center',
+}
+
+const bannerStyle = {
+    width: '500px',
+    height: '142px',
+    display: 'flex',
+    margin: '0 auto'
 }
 
 const searchStyle = {
