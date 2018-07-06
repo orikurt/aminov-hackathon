b9be6413acc9cef3dbb0c2c7398a59fb8f975d26
orikurt <orikball@gmail.com>
2018-07-07 00:15:04 +0300

    add some bullshit data to player card

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -1,8 +1,8 @@
 import React from 'react';
+import { numberFormat } from '../utils/format';
 
 const playerCardStyle = {
     display: 'flex',
-    // flexDirection: 'column'
 }
 
 const imageStyle = {
@@ -11,13 +11,25 @@ const imageStyle = {
     borderRadius: '3px'
 }
 
+const infoStyle = {
+    padding: '0 15px', 
+    display: 'flex',
+    flexDirection: 'column',
+    lineHeight: '30px'
+}
+
 const playerCard = (props) => {
+    console.log(props)
     return (
         <div style={playerCardStyle}>
             <img src={`/${props.player.image_url}`} alt={props.player.name} style={imageStyle} />
-            <div style={{padding: '5px'}}>
-                <h3>{props.player.name}</h3>
-                {props.stock ? <h3>{props.stock.price}$</h3> : null}
+            <div style={ infoStyle }>
+                { props.player ? <label style={{fontSize: '140%'}}>{props.player.name}</label> : null }
+                { props.stock ? <label style={{fontSize: '120%'}}>${props.stock.price}</label> : null}
+                { props.player ? <label>{props.player.team}</label> : null }
+                { props.player ? <label>#{props.player.number} {props.player.height} {props.player.weight}</label> : null }
+                { props.stock ? <label><span style={{color: '#aaa'}}>Total Shares </span>{numberFormat.format(props.stock.shares)}</label> : null }
+                { props.player ? <label><span style={{color: '#aaa'}}>Experience </span>{props.player.experience}</label> : null }
             </div>
         </div>
     )
